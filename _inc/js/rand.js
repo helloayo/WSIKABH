d5c424a7fcf0b1fb53da017ca1984ed4919317c6
helloayo <findayo@gmail.com>
2017-06-22 00:37:10 -0700

    moving this

---
+++ _inc/js/rand.js
@@ -0,0 +1,147 @@
+var text = new Array();
+
+text[1] = "Signed the Lilly Ledbetter Fair Pay Act, restoring basic protections against pay discrimination for women and other workers";
+text[2] = "Provided travel expenses to families of fallen soldiers to be on hand when the body arrives at Dover AFB";
+text[3] = "Reversed the policy of barring media coverage during the return of fallen soldiers to Dover Air Force Base";
+text[4] = "Launched recovery.gov to track spending from the Recovery Act, providing transparency and allowing the public to report fraud, waste, or abuse";
+text[5] = "Provided the Department of Veterans Affairs with more than $1.4 billion to improve services to America's Veterans";
+text[6] = "Signed the Children's Health Insurance Reauthorization Act, which provides health care to 11 million kids -- 4 million of whom were previously uninsured";
+text[7] = "Issued executive order to repeal Bush era restrictions on federal funding for embryonic stem cell research";
+text[8] = "Signed the Christopher and Dana Reeve Paralysis Act, the first piece of comprehensive legislation aimed at improving the lives of Americans living with paralysis";
+text[9] = "Developed stimulus package, which includes approx. $18 billion for nondefense scientific research and development";
+text[10] = "Signed the Weapons Systems Acquisition Reform Act to stop fraud and wasteful spending in the defense procurement and contracting system";
+text[11] = "Issued executive order to close the prison at Guantanamo Bay";
+text[12] = "Ended Bush administration's CIA program of 'enhanced interrogation methods' by requiring that the Army field manual be used as the guide for terrorism interrogations";
+text[13] = "Increased minority access to capital";
+text[14] = "Established Credit Card Bill of Rights, preventing credit card companies from imposing arbitrary rate increases on customers";
+text[15] = "Health Care Reform Bill, preventing insurance companies from denying insurance because of a pre-existing condition";
+text[16] = "Health Care Reform Bill, allowing children to remain covered by their parents' insurance until the age of 26";
+text[17] = "Tax cuts for up to 3.5 million small businesses to help pay for employee health care coverage";
+text[18] = "Tax credits for up to 29 million individuals to help pay for health insurance";
+text[19] = "Expansion of Medicaid to all individuals under age 65 with incomes up to 133 percent of the federal poverty level";
+text[20] = "Require health insurance plans to disclose how much of the premium actually goes to patient care";
+text[21] = "Added 4.6 billion USD to the Veterans Administration budget to recruit and retain more mental health professionals";
+text[22] = "Significantly increased funding for the Violence Against Women Act";
+text[23] = "Lifted restrictions granting Cuban Americans unrestricted rights to visit family and send remittances to the island";
+text[24] = "Eliminated subsidies to private lender middlemen of student loans and protect student borrowers";
+text[25] = "Increased funding for national parks and forests by 10%";
+text[26] = "Significantly expanded Pell grants, which help low-income students pay for college";
+text[27] = "Expanded hate crime law in the US to include sexual orientation through the Matthew Shepard and James Byrd Jr. Hate Crimes Prevention Act";
+text[28] = "Provided stimulus funding to boost private sector spaceflight programs";
+text[29] = "Appointed nation's first Chief Technology Officer";
+text[30] = "Signed financial reform law establishing a Consumer Financial Protection Bureau to look out for the interests of everyday Americans";
+text[31] = "Signed financial reform law requiring lenders to verify applicants' credit history, income, and employment status";
+text[32] = "Signed financial reform law prohibiting banks from engaging in proprietary trading (trading the bank's own money to turn a profit, often in conflict with their customers' interests)";
+text[33] = "Signed financial reform law allowing shareholders of publicly traded companies to vote on executive pay";
+text[34] = "Cut prescription drug cost for medicare recipients by 50%";
+text[35] = "Provided $12.2 Billion in new funding for Individuals With Disabilities Education Act";
+text[36] = "Extended Benefits to same-sex partners of federal employees";
+text[37] = "Appointed more openly gay officials than any other president in US history";
+text[38] = "The American Recovery & Reinvestment Act of 2009: a $789 billion economic stimulus plan";
+text[39] = "Created more private sector jobs in 2010 than during entire Bush years";
+text[40] = "Voluntary disclosure of White House visitors for the first time in US history";
+text[41] = "Appointed first Latina to the US Supreme Court";
+text[42] = "Promoted social responsibility through creation of serve.gov, a national database of volunteer opportunities";
+text[43] = "Reversed 'global gag rule', allowing US aid to go to organizations regardless of whether they provide abortions";
+text[44] = "Signed the Family Smoking Prevention and Tobacco Control Act, giving the FDA the authority to regulate the manufacturing, marketing, and sale of tobacco for the first time ";
+text[45] = "Signed New START Treaty - nuclear arms reduction pact with Russia";
+text[46] = "Increased average fuel economy standards from 27.5mpg to 35.5mpg, starting in 2016";
+
+
+var footnotes = new Array();
+footnotes[1] = "http://en.wikipedia.org/wiki/Lilly_Ledbetter_Fair_Pay_Act_of_2009";
+footnotes[2] = "http://thelede.blogs.nytimes.com/2009/03/18/pentagon-will-help-families/";
+footnotes[3] = "http://news.bbc.co.uk/2/hi/americas/7986203.stm";
+footnotes[4] = "http://recovery.gov";
+footnotes[5] = "http://www4.va.gov/budget/products.asp";
+footnotes[6] = "http://themiddleclass.org/bill/children039s-health-insurance-program-reauthorization-act-2009";
+footnotes[7] = "http://www.guardian.co.uk/world/2009/mar/09/obama-administration-stem-cell-funding";
+footnotes[8] = "http://www.christopherreeve.org/site/c.ddJFKRNoFiG/b.4506337/apps/s/content.asp?ct=6866203";
+footnotes[9] = "http://www.nytimes.com/2009/10/18/science/18sfstimulus.html";
+footnotes[10] = "http://en.wikipedia.org/wiki/Weapon_Systems_Acquisition_Reform_Act_of_2009";
+footnotes[11] = "http://www.cnn.com/2009/POLITICS/01/22/guantanamo.order/index.html";
+footnotes[12] = "http://www.cnn.com/2009/POLITICS/01/22/guantanamo.order/index.html";
+footnotes[13] = "http://www.sba.gov/idc/groups/public/documents/sba_homepage/news_release_09-58.pdf";
+footnotes[14] = "http://politifact.com/truth-o-meter/promises/promise/33/establish-a-credit-card-bill-of-rights/";
+footnotes[15] = "http://thecaucus.blogs.nytimes.com/2010/03/30/obama-signs-bill-on-student-loans-health-care/";
+footnotes[16] = "http://thecaucus.blogs.nytimes.com/2010/03/30/obama-signs-bill-on-student-loans-health-care/";
+footnotes[17] = "http://thecaucus.blogs.nytimes.com/2010/03/30/obama-signs-bill-on-student-loans-health-care/";
+footnotes[18] = "http://thecaucus.blogs.nytimes.com/2010/03/30/obama-signs-bill-on-student-loans-health-care/";
+footnotes[19] = "http://www.nytimes.com/2010/03/22/your-money/health-insurance/22consumer.html?_r=1&ref=policy";
+footnotes[20] = "http://thecaucus.blogs.nytimes.com/2010/03/30/obama-signs-bill-on-student-loans-health-care/";
+footnotes[21] = "http://www4.va.gov/budget/products.asp";
+footnotes[22] = "http://www.justice.gov/jmd/2010summary/html/ovw-bud-summary.htm";
+footnotes[23] = "http://www.npr.org/templates/story/story.php?storyId=103030021";
+footnotes[24] = "http://www.smartmoney.com/personal-finance/college-planning/the-health-care-bill-and-your-student-loans/?print=1";
+footnotes[25] = "http://www.npca.org/media_center/press_releases/2009/park_funding_102909.html";
+footnotes[26] = "http://thecaucus.blogs.nytimes.com/2010/03/30/obama-signs-bill-on-student-loans-health-care/";
+footnotes[27] = "http://www.hrc.org/13699.htm";
+footnotes[28] = "http://www.commercialspaceflight.org/?p=458";
+footnotes[29] = "http://www.whitehouse.gov/administration/eop/ostp";
+footnotes[30] = "http://www.cbsnews.com/8301-503544_162-20008835-503544.html";
+footnotes[31] = "http://www.cbsnews.com/8301-503544_162-20008835-503544.html";
+footnotes[32] = "http://www.cbsnews.com/8301-503544_162-20008835-503544.html";
+footnotes[33] = "http://www.cbsnews.com/8301-503544_162-20008835-503544.html";
+footnotes[34] = "http://www.govtrack.us/congress/bill.xpd?bill=h111-4872&tab=summary";
+footnotes[35] = "http://www2.ed.gov/policy/gen/leg/recovery/factsheet/idea.html";
+footnotes[36] = "http://www.whitehouse.gov/the-press-office/presidential-memorandum-extension-benefits-same-sex-domestic-partners-federal-emplo";
+footnotes[37] = "http://www.washingtontimes.com/news/2010/oct/26/obama-has-appointed-most-us-gay-officials/";
+footnotes[38] = "http://www.gpo.gov/fdsys/pkg/PLAW-111publ5/pdf/PLAW-111publ5.pdf";
+footnotes[39] = "http://newsjunkiepost.com/2010/10/08/its-official-more-private-sector-jobs-created-in-2010-than-during-entire-bush-years/";
+footnotes[40] = "http://www.whitehouse.gov/briefing-room/disclosures/visitor-records";
+footnotes[41] = "http://www.npr.org/templates/story/story.php?storyId=104648062";
+footnotes[42] = "http://www.serve.gov/";
+footnotes[43] = "http://www.guardian.co.uk/world/2009/jan/23/barack-obama-foreign-abortion-aid";
+footnotes[44] = "http://frwebgate.access.gpo.gov/cgi-bin/getdoc.cgi?dbname=111_cong_bills&docid=f:h1256enr.txt.pdf";
+footnotes[45] = "http://www.nytimes.com/2010/04/09/world/europe/09prexy.html?_r=1";
+footnotes[46] = "http://www.washingtonpost.com/wp-dyn/content/article/2010/04/01/AR2010040101412.html";
+
+var quips = [];
+quips[0] = "Big fucking deal. What else?";
+quips[1] = "But what has he done for me lately?";
+quips[2] = "I'm unimpressed.";
+quips[3] = "I could do that in my sleep.";
+quips[4] = quips[0];
+quips[5] = quips[0];
+quips[6] = quips[0];
+quips[7] = quips[0];
+quips[8] = "That doesn't affect me personally so who cares.";
+quips[9] = "Yeah, AND?";
+quips[10] = quips[0];
+quips[11] = quips[0];
+quips[12] = "That's cool, I guess.  What else you got?";
+
+var cursor = 1 + Math.floor((text.length - 1)*Math.random());
+$(setText());
+
+
+/*
+function loadText() {
+
+	var regex = new RegExp("\\Wq=(\\d+)");
+	var res = regex.exec($(location).attr('href'));	
+	if (res == null) {
+	    setText(null);
+	} else {
+		setText(res[1]);
+	}
+	
+}
+*/
+function setText(index) {
+	if (index == null || index < 1) {
+		index = cursor;
+	} 
+	
+	$('#fact').html(text[index]);
+	$('#source').html('<a target="_blank" href="' + footnotes[index] + '">' + footnotes[index] + '</a>');
+	$('#index').html('#' + index);
+	$('#buttons').fact( quips[Math.floor(Math.random()*quips.length)] );
+	
+	if(cursor >= text.length -1){
+		cursor = 1;
+	}
+	else{
+		cursor++;
+	}
+}
